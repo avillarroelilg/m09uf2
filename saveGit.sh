a7f0f5e2caf2bc2356437c9c5ae8d5a9a7bc80d5
Adrian Villarroel <avillarroel@ilg.cat>
2022-02-08 19:32:17 +0100

    first commit

---
+++ saveGit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# empezamos !!!
+echo "<< Hola!!,comencemos ^-^ >>"
+echo "		escribe el commit >>:"
+read commit
+if [ -z "$commit" ];
+	then
+		echo "tiene que pasarme un el texto del commit :("
+
+else
+	#echo " >>se ejecuta el script: ,$commit"
+	git add . && git commit -a -m "commit:$commit" && git push
+	#git add . && git commit -m"commit:$commit"
+fi
