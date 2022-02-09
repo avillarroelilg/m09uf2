019f1d608e693832a2e59c001ae74ffb1505b650
Adrian Villarroel <avillarroel@ilg.cat>
2022-02-09 17:36:09 +0100

    commit:paquete ej1 add

---
+++ Exercici_1_Threads_implements_Runnable/IKSRotarranConstants.java
@@ -0,0 +1,6 @@
+package Exercici_1_Threads_implements_Runnable;
+
+public interface IKSRotarranConstants {
+	String[] LLOCS_DE_SERVEI = {"", "pont", "enginyeria", "cuina", "infermeria", "sala d'armes"};
+	String[] DEPARTAMENTS = {"", "comandament", "armes", "timó i navegació", "enginyeria", "ciència"};
+}
