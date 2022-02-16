c39670c36c855e18104efba63d2e839f12576c3c
avillarroelilg <avillarroel@ilg.cat>
2022-02-16 22:08:38 +0100

    commit:ej1 terminado

--- Ej_1_Runnables/CapDeDepartament.java
+++ Ej_1_Runnables/CapDeDepartament.java
@@ -1,24 +1,32 @@
 package Ej_1_Runnables;
 
+import Ej_1_Runnables.Magatzem;
+
 public class CapDeDepartament implements Runnable {
 	private Magatzem magatzem;
 	int movimentRacions;
 	
+	public CapDeDepartament(Magatzem magatzemTmp, int movimentRacions) {
+		this.magatzem = magatzemTmp;
+		this.movimentRacions = movimentRacions;
+	}
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		String nomFil = Thread.currentThread().getName();
 		System.out.println("    " + nomFil + ".INICI\n "
-				+ "\\s.movimentRacions = "  + movimentRacions
-				+ "\n\\s.magatzem.comprovarQuantitatRacions() = "  + magatzem.comprovarQuantitatRacions());
+				+ "   .movimentRacions = "  + movimentRacions
+				+ "\n   .magatzem.comprovarQuantitatRacions() = "  + magatzem.comprovarQuantitatRacions());
 		
         System.out.println("    " + nomFil + ".REALITZO L'OPERACIÓ");
 		
 		if (movimentRacions > 0) {
         	magatzem.retornarRacions(movimentRacions);
+        	//System.out.println("#### retornar ####");
         } else {
         	magatzem.agafaRacions(movimentRacions);
+        	//System.out.println("#### agafar ####");
         }
 		System.out.println("    " + nomFil + ".magatzem.comprovarQuantitatRacions() = "  + magatzem.comprovarQuantitatRacions());
         System.out.println("    " + nomFil + ".FI");
